4beac269a8aabccb46aa4e308eaaf507f17c3747
wlfrii <longfei.wang@sjtu.edu.cn>
2022-06-27 20:42:40 +0800

    specify the precision by a macro

---
+++ export/kine/kine_precision.h
@@ -0,0 +1,38 @@
+/**--------------------------------------------------------------------
+ *
+ *   				   Mathematics extension library
+ *
+ * Description:
+ * This file is part of lib_math. You can redistribute it and or modify
+ * it to construct your own project. It is wellcome to use this library
+ * in your scientific research work.
+ *
+ * @file 		kine_precision.h
+ *
+ * @brief 		Control the precision when kinematics-related interfaces.
+ *
+ * @author		Longfei Wang
+ *
+ * @date		2022/06/27
+ *
+ * @license
+ *
+ * Copyright (C) 2022 Longfei Wang.
+ *
+ * --------------------------------------------------------------------
+ * Change History:
+ *
+ * -------------------------------------------------------------------*/
+#ifndef LIB_MATH_KINE_PRECISION_H_LF
+#define LIB_MATH_KINE_PRECISION_H_LF
+
+namespace mmath{
+
+#ifdef LIB_MATH_KINE_DOUBLE
+using kfloat = double;
+#else
+using kfloat = float;
+#endif
+
+} //namespace::mmath
+#endif // LIB_MATH_KINE_PRECISION_H_LF
