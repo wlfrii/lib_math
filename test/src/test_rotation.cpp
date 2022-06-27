09e81e05247bbc382e7d112cf9057410847557a3
wlfrii <longfei.wang@sjtu.edu.cn>
2022-06-27 15:20:57 +0800

    update

--- test/src/test_rotation.cpp
+++ test/src/test_rotation.cpp
@@ -30,18 +30,21 @@ TEST_CASE("Test drotation", "[drotation]")
     Eigen::Matrix3f rot, drot;
     float radian = mmath::deg2radf(30);
     drot = mmath::dRotByXf(radian);
+    CHECK(drot(0,0) == Approx(0).margin(1e-7));
     CHECK(drot(1,1) == Approx(-sin(radian)).margin(1e-7));
     CHECK(drot(1,2) == Approx(-cos(radian)).margin(1e-7));
     CHECK(drot(2,1) == Approx(-drot(1,2)).margin(1e-7));
     CHECK(drot(2,2) == Approx(drot(1,1)).margin(1e-7));
 
     drot = mmath::dRotByYf(radian);
+    CHECK(drot(1,1) == Approx(0).margin(1e-7));
     CHECK(drot(0,0) == Approx(-sin(radian)).margin(1e-7));
     CHECK(drot(0,2) == Approx(cos(radian)).margin(1e-7));
     CHECK(drot(2,0) == Approx(-drot(0,2)).margin(1e-7));
     CHECK(drot(2,2) == Approx(drot(0,0)).margin(1e-7));
 
     drot = mmath::dRotByZf(radian);
+    CHECK(drot(2,2) == Approx(0).margin(1e-7));
     CHECK(drot(0,0) == Approx(-sin(radian)).margin(1e-7));
     CHECK(drot(0,1) == Approx(-cos(radian)).margin(1e-7));
     CHECK(drot(1,0) == Approx(-drot(0,1)).margin(1e-7));
