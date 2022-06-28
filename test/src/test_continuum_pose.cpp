bd61df9aa932b3d49087e65db330c522853ae96c
wlfrii <longfei.wang@sjtu.edu.cn>
2022-06-28 11:23:46 +0800

    add catch2 test

--- test/src/test_continuum_pose.cpp
+++ test/src/test_continuum_pose.cpp
@@ -69,34 +69,137 @@ TEST_CASE("Test continuum pose with Lr", "[continuum]")
 }
 
 
-TEST_CASE("Test continuum dpose", "[continuum]")
+TEST_CASE("Test continuum dpose2theta", "[continuum]")
 {
     float L = 30;
     float theta = mmath::deg2rad(30);
     float delta = mmath::deg2rad(50);
     mmath::Pose pose = mmath::continuum::dSingleSegmentPose2theta(L, theta, delta);
 
-    float val = powf(cos(delta), 2)*cos(theta) + powf(sin(delta), 2);
+    float val = -powf(cos(delta), 2)*sin(theta);
     CHECK(pose.R(0,0) == Approx(val).margin(1e-6));
-    val = sin(delta)*cos(delta)*(cos(theta) - 1);
+    val = -sin(delta)*cos(delta)*sin(theta);
     CHECK(pose.R(0,1) == Approx(val).margin(1e-6));
-    val = cos(delta)*sin(theta);
+    val = cos(delta)*cos(theta);
     CHECK(pose.R(0,2) == Approx(val).margin(1e-6));
 
     CHECK(pose.R(1,0) == Approx(pose.R(0,1)).margin(1e-6));
-    val = powf(sin(delta), 2)*cos(theta) + powf(cos(delta), 2);
+    val = -powf(sin(delta), 2)*sin(theta);
     CHECK(pose.R(1,1) == Approx(val).margin(1e-6));
-    val = sin(delta) * sin(theta);
+    val = sin(delta) * cos(theta);
     CHECK(pose.R(1,2) == Approx(val).margin(1e-6));
 
     CHECK(pose.R(2,0) == Approx(-pose.R(0,2)).margin(1e-6));
     CHECK(pose.R(2,1) == Approx(-pose.R(1,2)).margin(1e-6));
-    CHECK(pose.R(2,2) == Approx(cos(theta)).margin(1e-6));
+    CHECK(pose.R(2,2) == Approx(-sin(theta)).margin(1e-6));
 
-    val = L / theta * cos(delta) * (1 - cos(theta));
+    val = L * cos(delta) * (theta*sin(theta) - 1 + cos(theta)) / (theta*theta);
     CHECK(pose.t[0] == Approx(val).margin(1e-6));
-    val = L / theta * sin(delta) * (1 - cos(theta));
+    val = L * sin(delta) * (theta*sin(theta) - 1 + cos(theta)) / (theta*theta);
     CHECK(pose.t[1] == Approx(val).margin(1e-6));
-    val = L / theta * sin(theta);
+    val = L * (theta*cos(theta) - sin(theta)) / (theta*theta);
     CHECK(pose.t[2] == Approx(val).margin(1e-6));
+}
+
+
+TEST_CASE("Test continuum dpose2theta with Lr", "[continuum]")
+{
+    float L = 30;
+    float Lr = 10;
+    float theta = mmath::deg2rad(30);
+    float delta = mmath::deg2rad(50);
+    mmath::Pose pose = mmath::continuum::dSingleWithRigidSegmentPose2theta(
+                L, theta, delta, Lr);
+
+    float val = -powf(cos(delta), 2)*sin(theta);
+    CHECK(pose.R(0,0) == Approx(val).margin(1e-6));
+    val = -sin(delta)*cos(delta)*sin(theta);
+    CHECK(pose.R(0,1) == Approx(val).margin(1e-6));
+    val = cos(delta)*cos(theta);
+    CHECK(pose.R(0,2) == Approx(val).margin(1e-6));
+
+    CHECK(pose.R(1,0) == Approx(pose.R(0,1)).margin(1e-6));
+    val = -powf(sin(delta), 2)*sin(theta);
+    CHECK(pose.R(1,1) == Approx(val).margin(1e-6));
+    val = sin(delta) * cos(theta);
+    CHECK(pose.R(1,2) == Approx(val).margin(1e-6));
+
+    CHECK(pose.R(2,0) == Approx(-pose.R(0,2)).margin(1e-6));
+    CHECK(pose.R(2,1) == Approx(-pose.R(1,2)).margin(1e-6));
+    CHECK(pose.R(2,2) == Approx(-sin(theta)).margin(1e-6));
+
+    val = L * cos(delta) * (theta*sin(theta) - 1 + cos(theta)) / (theta*theta) +
+            Lr * cos(delta) * cos(theta);
+    CHECK(pose.t[0] == Approx(val).margin(1e-6));
+    val = L * sin(delta) * (theta*sin(theta) - 1 + cos(theta)) / (theta*theta) +
+            Lr * sin(delta) * cos(theta);
+    CHECK(pose.t[1] == Approx(val).margin(1e-6));
+    val = L * (theta*cos(theta) - sin(theta)) / (theta*theta) - Lr*sin(theta);
+    CHECK(pose.t[2] == Approx(val).margin(1e-6));
+}
+
+
+TEST_CASE("Test continuum dpose2delta", "[continuum]")
+{
+    float L = 30;
+    float theta = mmath::deg2rad(30);
+    float delta = mmath::deg2rad(50);
+    mmath::Pose pose = mmath::continuum::dSingleSegmentPose2delta(L, theta, delta);
+
+    float val = 2*cos(delta)*(-sin(delta))*cos(theta) + 2*sin(delta)*cos(delta);
+    CHECK(pose.R(0,0) == Approx(val).margin(1e-6));
+    val = (cos(delta)*cos(delta) - sin(delta)*sin(delta) )*(cos(theta) - 1);
+    CHECK(pose.R(0,1) == Approx(val).margin(1e-6));
+    val = -sin(delta)*sin(theta);
+    CHECK(pose.R(0,2) == Approx(val).margin(1e-6));
+
+    CHECK(pose.R(1,0) == Approx(pose.R(0,1)).margin(1e-6));
+    val = 2*sin(delta)*cos(delta)*cos(theta) - 2*cos(delta)*sin(delta);
+    CHECK(pose.R(1,1) == Approx(val).margin(1e-6));
+    val = cos(delta) * sin(theta);
+    CHECK(pose.R(1,2) == Approx(val).margin(1e-6));
+
+    CHECK(pose.R(2,0) == Approx(-pose.R(0,2)).margin(1e-6));
+    CHECK(pose.R(2,1) == Approx(-pose.R(1,2)).margin(1e-6));
+    CHECK(pose.R(2,2) == Approx(0).margin(1e-6));
+
+    val = L / theta * (-sin(delta)) * (1 - cos(theta));
+    CHECK(pose.t[0] == Approx(val).margin(1e-6));
+    val = L / theta * cos(delta) * (1 - cos(theta));
+    CHECK(pose.t[1] == Approx(val).margin(1e-6));
+    CHECK(pose.t[2] == Approx(0).margin(1e-6));
+}
+
+
+TEST_CASE("Test continuum dpose2delta with Lr", "[continuum]")
+{
+    float L = 30;
+    float Lr = 10;
+    float theta = mmath::deg2rad(30);
+    float delta = mmath::deg2rad(50);
+    mmath::Pose pose = mmath::continuum::dSingleWithRigidSegmentPose2delta(
+                L, theta, delta, Lr);
+
+    float val = 2*cos(delta)*(-sin(delta))*cos(theta) + 2*sin(delta)*cos(delta);
+    CHECK(pose.R(0,0) == Approx(val).margin(1e-6));
+    val = (cos(delta)*cos(delta) - sin(delta)*sin(delta) )*(cos(theta) - 1);
+    CHECK(pose.R(0,1) == Approx(val).margin(1e-6));
+    val = -sin(delta)*sin(theta);
+    CHECK(pose.R(0,2) == Approx(val).margin(1e-6));
+
+    CHECK(pose.R(1,0) == Approx(pose.R(0,1)).margin(1e-6));
+    val = 2*sin(delta)*cos(delta)*cos(theta) - 2*cos(delta)*sin(delta);
+    CHECK(pose.R(1,1) == Approx(val).margin(1e-6));
+    val = cos(delta) * sin(theta);
+    CHECK(pose.R(1,2) == Approx(val).margin(1e-6));
+
+    CHECK(pose.R(2,0) == Approx(-pose.R(0,2)).margin(1e-6));
+    CHECK(pose.R(2,1) == Approx(-pose.R(1,2)).margin(1e-6));
+    CHECK(pose.R(2,2) == Approx(0).margin(1e-6));
+
+    val = L/theta * (-sin(delta)) * (1-cos(theta)) + Lr*(-sin(delta))*sin(theta);
+    CHECK(pose.t[0] == Approx(val).margin(1e-6));
+    val = L/theta * cos(delta) * (1-cos(theta)) + Lr*cos(delta)*sin(theta);
+    CHECK(pose.t[1] == Approx(val).margin(1e-6));
+    CHECK(pose.t[2] == Approx(0).margin(1e-6));
 }
--- test/src/test_rotation.cpp
+++ test/src/test_rotation.cpp
@@ -27,7 +27,7 @@ TEST_CASE("Test rotation", "[rotation]")
 
 TEST_CASE("Test drotation", "[drotation]")
 {
-    Eigen::Matrix3f rot, drot;
+    Eigen::Matrix3f drot;
     float radian = mmath::deg2radf(30);
     drot = mmath::dRotByXf(radian);
     CHECK(drot(0,0) == Approx(0).margin(1e-7));
@@ -49,4 +49,37 @@ TEST_CASE("Test drotation", "[drotation]")
     CHECK(drot(0,1) == Approx(-cos(radian)).margin(1e-7));
     CHECK(drot(1,0) == Approx(-drot(0,1)).margin(1e-7));
     CHECK(drot(1,1) == Approx(drot(0,0)).margin(1e-7));
+}
+
+
+TEST_CASE("Test drotation2delta", "[drotation]")
+{
+    Eigen::Matrix3f drot;
+    float delta = mmath::deg2radf(30);
+    Eigen::Matrix3f R_t1_2_tb =
+            mmath::rotByZ<float>(-mmath::PI / 2)*
+            mmath::rotByZ<float>(delta)*
+            mmath::rotByY<float>(-mmath::PI / 2);
+    CHECK(R_t1_2_tb(0,1) == Approx(cos(delta)).margin(1e-7));
+    CHECK(R_t1_2_tb(0,2) == Approx(-sin(delta)).margin(1e-7));
+    CHECK(R_t1_2_tb(1,1) == Approx(-R_t1_2_tb(0,2)).margin(1e-7));
+    CHECK(R_t1_2_tb(1,2) == Approx(R_t1_2_tb(0,1)).margin(1e-7));
+    CHECK(R_t1_2_tb(2,0) == Approx(1).margin(1e-7));
+
+    Eigen::Matrix3f dR_t1_2_tb =
+            mmath::rotByZ<float>(-mmath::PI / 2)*
+            mmath::dRotByZ<float>(delta)*
+            mmath::rotByY<float>(-mmath::PI / 2);
+    CHECK(dR_t1_2_tb(0,1) == Approx(-sin(delta)).margin(1e-7));
+    CHECK(dR_t1_2_tb(0,2) == Approx(-cos(delta)).margin(1e-7));
+    CHECK(dR_t1_2_tb(1,1) == Approx(-dR_t1_2_tb(0,2)).margin(1e-7));
+    CHECK(dR_t1_2_tb(1,2) == Approx(dR_t1_2_tb(0,1)).margin(1e-7));
+    CHECK(dR_t1_2_tb(2,0) == Approx(0).margin(1e-7));
+
+    Eigen::Matrix3f dR_te_2_te =dR_t1_2_tb.transpose();
+    CHECK(dR_te_2_te(1,0) == Approx(-sin(delta)).margin(1e-7));
+    CHECK(dR_te_2_te(2,0) == Approx(-cos(delta)).margin(1e-7));
+    CHECK(dR_te_2_te(1,1) == Approx(-dR_te_2_te(2,0)).margin(1e-7));
+    CHECK(dR_te_2_te(2,1) == Approx(dR_te_2_te(1,0)).margin(1e-7));
+    CHECK(dR_te_2_te(0,2) == Approx(0).margin(1e-7));
 }
