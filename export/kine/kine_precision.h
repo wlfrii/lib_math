b8190993a1b8c0c1806fa12e50a1f58505535ca0
wlfrii <longfei.wang@sjtu.edu.cn>
2022-06-28 12:07:03 +0800

    update

--- export/kine/kine_precision.h
+++ export/kine/kine_precision.h
@@ -29,8 +29,9 @@
 namespace mmath{
 
 /**
- * @brief To use double precision when using ./kine, define this MACRO in 
- * your preprocessing list before include <lib_math/lib_math.h>
+ * @brief To use double precision when using ./kine, define this
+ * LIB_MATH_KINE_DOUBLE
+ * macro in your preprocessing list before include <lib_math/lib_math.h>.
  */
 #ifdef LIB_MATH_KINE_DOUBLE
 using kfloat = double;
