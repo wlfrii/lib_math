552242f75130d93b4c8237326c1d129ca064f1d9
wlfrii <longfei.wang@sjtu.edu.cn>
2024-03-27 16:14:52 +0800

    update doxygen comments

--- export/lib_math.h
+++ export/lib_math.h
@@ -25,42 +25,43 @@
  * #v1.1 Complete kinematics related utilities.
  * #v1.2 Templated most of the interfaces.
  * #v1.3 Fixed several bug and integrate timer.
+ * #v1.4 All the comments are updated.
  * 
  * -------------------------------------------------------------------*/
 #ifndef LIB_MATH_LIB_LF
 #define LIB_MATH_LIB_LF
 
-// Precision
+/** Precision */
 #include "lib_math/math_precision.h"
 
-// Tiny utilities
+/** Tiny utilities */
 #include "lib_math/util/angle.h"
 #include "lib_math/util/linspace.h"
 
-// Matrix related utilities
+/** Matrix related utilities */
 #include "lib_math/matrix/mat.h"
 #include "lib_math/matrix/rotation.h"
 #include "lib_math/matrix/skew.h"
 #include "lib_math/matrix/drotation.h"
 
-// Kinematics related utilities
-#include "lib_math/lib_math/kine/pose.h"
+/** Kinematics related utilities */
+#include "lib_math/kine/pose.h"
 #include "lib_math/kine/continuum_configspc.h"
 #include "lib_math/kine/continuum_pose.h"
 #include "lib_math/kine/dcontinuum_pose.h"
 
-// Curve related utilities
+/** Curve related utilities */
 #include "lib_math/curve/line_2d.h"
 #include "lib_math/curve/gauss_curve_2d.h"
 
 
-// Include some useful function for time counting.
-// Although timer counting is not really realted to math, this file contains
-// several pretty useful function for test the time comsumption.
+/** Include some useful function for time counting. <p>
+ * Although timer counting is not really realted to math, this file contains <p>
+ * several pretty useful function for test the time comsumption. */
 #include "lib_math/timer/timer.h"
 
 
-// Camera projection
+/** Camera projection */
 #include "lib_math/cam/camera_projector.h"
 
 // Some explicit template class
