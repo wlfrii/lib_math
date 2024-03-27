11a8210e214cd48b014f1694400b1338dae4e295
wlfrii <longfei.wang@sjtu.edu.cn>
2024-03-27 14:59:54 +0800

    update doxygen comments; add void-returned function

--- export/lib_math/cam/camera_projector.h
+++ export/lib_math/cam/camera_projector.h
@@ -30,12 +30,10 @@
 
 namespace mmath{
 
-/** @brief cam namespace
- */
+/** A cam namespace to specify left and right camera */
 namespace cam
 {
-/** @brief Specify the camera index
- */
+/** Specify the camera index */
 using ID = bool;
 extern ID LEFT;
 extern ID RIGHT;
@@ -43,104 +41,289 @@ extern ID RIGHT;
 
 
 /**
- * @brief The CameraProjector class designed to the conversion between 3D point
- * w.r.t Camera frame and the 2D point w.r.t image frame.
+ * @brief The CameraProjector class is designed for the conversion between 3D 
+ * point w.r.t Camera frame and the 2D point w.r.t image frame.
+ * 
+ * @note There are several definitions should be clarified first. 
+ * - GLOBAL camera frame: equals to camera frame for monocular, or indicates the 
+ * frame in the middle between left and right cameras for binocular.
+ * - GLOBAL imaging frame: equals to image frame for monocular, or indicates the 
+ * frame in the middle between left and right imaging for binocular.
+ * - SPECIFIED camera frame: equals to camera frame for monocular, or indicates  
+ * the left or right camera frame for binocular.
+ * - SPECIFIED imaging frame: equals to image frame for monocular, or indicates  
+ * the left or right image frame for binocular.
  */
 class CameraProjector
 {
 public:
     /**
-     * @brief Construct a new Camera Projector object
+     * @brief Construct a new Camera Projector object.
      * 
-     * @param fxy  The focal length of camera. For binocular, this value should
-     * be the stereo-rectified focal length value.
-     * @param cx  The x coordinta of camera optical axis
-     * @param cy  The y coordinta of camera optical axis
-     * @param t   The distance between stereo cameras
+     * @param fxy The focal length of camera. For binocular, this value should
+     *            be the stereo-rectified value.
+     * @param cx  The x coordinta of camera optical axis. For binocular, this 
+     *            value should be the stereo-rectified value.
+     * @param cy  The y coordinta of camera optical axis. For binocular, this 
+     *            value should be the stereo-rectified value.
+     * @param t   The distance between stereo cameras. When no distance or zero
+     *            value is given, the camera is supposed to be a monocular.
      */
     CameraProjector(kfloat fxy, kfloat cx, kfloat cy, kfloat t = 0);
     ~CameraProjector();
 
 
     /**
-     * @brief Projecting 3D point to 2D w.r.t GLOBAL imaging frame
-     * @param x  X coordinate of the 3D point
-     * @param y  Y coordinate of the 3D point
-     * @param z  Z coordinate of the 3D point
-     * @return 2D point w.r.t GLOBAL imaging frame
+     * @brief Projecting 3D point to 2D w.r.t GLOBAL imaging frame.
+     * 
+     * @remark This is the base of overloaded member functions.
+     * 
+     * @param [in]  x  X coordinate of the 3D point w.r.t GLOBAL camera frame.
+     * @param [in]  y  Y coordinate of the 3D point w.r.t GLOBAL camera frame.
+     * @param [in]  z  Z coordinate of the 3D point w.r.t GLOBAL camera frame.
+     * @param [out] pt2D A 2D point w.r.t GLOBAL imaging frame.
+     */
+    void cvt3Dto2D(kfloat x, kfloat y, kfloat z, 
+                   Eigen::Vector<kfloat, 2>& pt2D) const;
+
+
+    /**
+     * @brief Projecting 3D point to 2D w.r.t GLOBAL imaging frame.
+     * 
+     * @remark This is an overloaded member function, provided for convenience. 
+     * It differs from the base function only in what argument(s) it accepts 
+     * and the returned value. To improve efficiency, the member function with 
+     * the void-returned value is suggested.
+     * 
+     * @param [in]  x  X coordinate of the 3D point w.r.t GLOBAL camera frame.
+     * @param [in]  y  Y coordinate of the 3D point w.r.t GLOBAL camera frame.
+     * @param [in]  z  Z coordinate of the 3D point w.r.t GLOBAL camera frame.
+     * 
+     * @return A 2D point w.r.t GLOBAL imaging frame.
      */
     Eigen::Vector<kfloat, 2> cvt3Dto2D(kfloat x, kfloat y, kfloat z) const;
 
 
     /**
-     * @brief Projecting 3D point to 2D w.r.t GLOBAL imaging frame
-     * @param pt3D
-     * @param id  Specify the camera index for binocular
-     * @return 2D point w.r.t GLOBAL imaging frame
+     * @brief Projecting 3D point to 2D w.r.t GLOBAL imaging frame.
+     * 
+     * @remark This is an overloaded member function, provided for convenience. 
+     * It differs from the base function only in what argument(s) it accepts 
+     * and the returned value. 
+     * 
+     * @param [in]  pt3D A 3D point w.r.t GLOBAL camera frame.
+     * @param [out] pt2D A 2D point w.r.t GLOBAL imaging frame.
+     */
+    void cvt3Dto2D(const Eigen::Vector<kfloat, 3>& pt3D,
+                   Eigen::Vector<kfloat, 2>& pt2D) const;
+
+
+    /**
+     * @brief Projecting 3D point to 2D w.r.t GLOBAL imaging frame.
+     * 
+     * @remark This is an overloaded member function, provided for convenience. 
+     * It differs from the base function only in what argument(s) it accepts 
+     * and the returned value. To improve efficiency, the member function with 
+     * the void-returned value is suggested.
+     * 
+     * @param [in]  pt3D A 3D point w.r.t GLOBAL camera frame.
+     * 
+     * @return A 2D point w.r.t GLOBAL imaging frame.
      */
     Eigen::Vector<kfloat, 2> cvt3Dto2D(const Eigen::Vector<kfloat, 3>& pt3D) const;
 
 
     /**
-     * @brief Projecting 3D point to 2D w.r.t SPECIFIED imaging frame
-     * @param x  X coordinate of the 3D point
-     * @param y  Y coordinate of the 3D point
-     * @param z  Z coordinate of the 3D point
-     * @param id  Specify the camera index for binocular
-     * @return 2D point w.r.t SPECIFIED imaging frame
+     * @brief Projecting 3D point to 2D w.r.t SPECIFIED imaging frame.
+     * 
+     * @remark This is the base of overloaded member functions.
+     * 
+     * @param [in]  x  X coordinate of the 3D point w.r.t GLOBAL camera frame.
+     * @param [in]  y  Y coordinate of the 3D point w.r.t GLOBAL camera frame.
+     * @param [in]  z  Z coordinate of the 3D point w.r.t GLOBAL camera frame.
+     * @param [in]  id  Specify the camera index for binocular.
+     * @param [out] pt2D A 2D point w.r.t SPECIFIED imaging frame.
+     */
+    void cvt3Dto2D(kfloat x, kfloat y, kfloat z, cam::ID id,
+                   Eigen::Vector<kfloat, 2>& pt2D) const;
+
+
+    /**
+     * @brief Projecting 3D point to 2D w.r.t SPECIFIED imaging frame.
+     * 
+     * @remark This is an overloaded member function, provided for convenience. 
+     * It differs from the base function only in what argument(s) it accepts 
+     * and the returned value. To improve efficiency, the member function with 
+     * the void-returned value is suggested.
+     * 
+     * @param [in]  x  X coordinate of the 3D point w.r.t GLOBAL camera frame.
+     * @param [in]  y  Y coordinate of the 3D point w.r.t GLOBAL camera frame.
+     * @param [in]  z  Z coordinate of the 3D point w.r.t GLOBAL camera frame.
+     * @param [in]  id  Specify the camera index for binocular.
+     * 
+     * @return A 2D point w.r.t SPECIFIED imaging frame.
      */
     Eigen::Vector<kfloat, 2> cvt3Dto2D(kfloat x, kfloat y, kfloat z,
                                        cam::ID id) const;
 
 
     /**
-     * @brief Projecting 3D point to 2D w.r.t SPECIFIED imaging frame
-     * @param pt3D
-     * @param id  Specify the camera index for binocular
-     * @return 2D point w.r.t SPECIFIED imaging frame
+     * @brief Projecting 3D point to 2D w.r.t SPECIFIED imaging frame.
+     * 
+     * @remark This is an overloaded member function, provided for convenience. 
+     * It differs from the base function only in what argument(s) it accepts 
+     * and the returned value. 
+     * 
+     * @param [in]  pt3D  A 3D point w.r.t GLOBAL camera frame.
+     * @param [in]  id    Specify the camera index for binocular.
+     * @param [out] pt2D  A 2D point w.r.t SPECIFIED imaging frame.
+     */
+    void cvt3Dto2D(const Eigen::Vector<kfloat, 3>& pt3D, cam::ID id,
+                   Eigen::Vector<kfloat, 2>& pt2D) const;
+
+
+    /**
+     * @brief Projecting 3D point to 2D w.r.t SPECIFIED imaging frame.
+     * 
+     * @remark This is an overloaded member function, provided for convenience. 
+     * It differs from the base function only in what argument(s) it accepts 
+     * and the returned value. To improve efficiency, the member function with 
+     * the void-returned value is suggested.
+     * 
+     * @param [in]  pt3D  A 3D point w.r.t GLOBAL camera frame.
+     * @param [in]  id    Specify the camera index for binocular.
+     * 
+     * @return A 2D point w.r.t SPECIFIED imaging frame.
      */
     Eigen::Vector<kfloat, 2> cvt3Dto2D(const Eigen::Vector<kfloat, 3>& pt3D,
                                        cam::ID id) const;
 
 
     /**
-     * @brief lifting 2D point that w.r.t GLOBAL camera frame to 3D
-     * @param u  U coordinate of the 2D point
-     * @param v  V coordinate of the 2D point
-     * @param depth   Depth of the given 2D point
-     * @return 3D point w.r.t GLOBAL camera frame
+     * @brief Lifting 2D point that w.r.t GLOBAL imaging frame to 3D.
+     * 
+     * @remark This is the base of overloaded member functions.
+     * 
+     * @param [in] u  U coordinate of the 2D point w.r.t GLOBAL imaging frame.
+     * @param [in] v  V coordinate of the 2D point w.r.t GLOBAL imaging frame.
+     * @param [in] depth Depth of the given 2D point w.r.t GLOBAL camera frame.
+     * @param [out] pt3D  A 3D point w.r.t GLOBAL camera frame.
+     */
+    void cvt2Dto3D(kfloat u, kfloat v, kfloat depth,
+                  Eigen::Vector<kfloat, 3>& pt3D) const;
+
+
+    /**
+     * @brief Lifting 2D point that w.r.t GLOBAL imaging frame to 3D.
+     * 
+     * @remark This is an overloaded member function, provided for convenience. 
+     * It differs from the base function only in what argument(s) it accepts 
+     * and the returned value. To improve efficiency, the member function with 
+     * the void-returned value is suggested.
+     * 
+     * @param [in] u  U coordinate of the 2D point w.r.t GLOBAL imaging frame.
+     * @param [in] v  V coordinate of the 2D point w.r.t GLOBAL imaging frame.
+     * @param [in] depth Depth of the given 2D point w.r.t GLOBAL camera frame.
+     * 
+     * @return A 3D point w.r.t GLOBAL camera frame
      */
     Eigen::Vector<kfloat, 3> cvt2Dto3D(kfloat u, kfloat v, kfloat depth) const;
 
 
     /**
-     * @brief lifting 2D point that w.r.t GLOBAL camera frame to 3D
-     * @param pt2D  2D Points w.r.t specifed camera
-     * @param depth  Depth of the given 2D point
-     * @return 3D point w.r.t GLOBAL camera frame
+     * @brief Lifting 2D point that w.r.t GLOBAL imaging frame to 3D
+     * 
+     * @remark This is an overloaded member function, provided for convenience. 
+     * It differs from the base function only in what argument(s) it accepts 
+     * and the returned value. 
+     * 
+     * @param [in] pt2D  A 2D point w.r.t GLOBAL imaging frame.
+     * @param [in] depth Depth of the given 2D point w.r.t GLOBAL camera frame.
+     * @param [out] pt3D  A 3D point w.r.t GLOBAL camera frame.
+     */
+    void cvt2Dto3D(const Eigen::Vector<kfloat, 2>& pt2D, kfloat depth,
+                   Eigen::Vector<kfloat, 3>& pt3D) const;
+
+
+    /**
+     * @brief Lifting 2D point that w.r.t GLOBAL imaging frame to 3D.
+     * 
+     * @remark This is an overloaded member function, provided for convenience. 
+     * It differs from the base function only in what argument(s) it accepts 
+     * and the returned value. To improve efficiency, the member function with 
+     * the void-returned value is suggested.
+     * 
+     * @param [in] pt2D  A 2D point w.r.t GLOBAL imaging frame.
+     * @param [in] depth Depth of the given 2D point w.r.t GLOBAL camera frame.
+     * 
+     * @return A 3D point w.r.t GLOBAL camera frame.
      */
     Eigen::Vector<kfloat, 3> cvt2Dto3D(const Eigen::Vector<kfloat, 2>& pt2D,
                                        kfloat depth) const;
 
 
     /**
-     * @brief lifting 2D point that w.r.t SPECIFIED camera frame to 3D
-     * @param u  U coordinate of the 2D point
-     * @param v  V coordinate of the 2D point
-     * @param depth   Depth of the given 2D point
-     * @param id  Specify the camera index for binocular
-     * @return 3D point w.r.t SPECIFIED camera frame
+     * @brief Lifting 2D point that w.r.t SPECIFIED imaging frame to 3D.
+     * 
+     * @remark This is the base of overloaded member functions.
+     * 
+     * @param [in] u U coordinate of the 2D point w.r.t SPECIFIED imaging frame.
+     * @param [in] v V coordinate of the 2D point w.r.t SPECIFIED imaging frame.
+     * @param [in] depth Depth of the given 2D point w.r.t GLOBAL camera frame.
+     * @param [in] id    Specify the camera index for binocular.
+     * @param [out] pt3D  A 3D point w.r.t GLOBAL camera frame.
+     */
+    void cvt2Dto3D(kfloat u, kfloat v, kfloat depth, cam::ID id, 
+                   Eigen::Vector<kfloat, 3>& pt3D) const;
+
+    /**
+     * @brief Lifting 2D point that w.r.t SPECIFIED imaging frame to 3D.
+     * 
+     * @remark This is an overloaded member function, provided for convenience. 
+     * It differs from the base function only in what argument(s) it accepts 
+     * and the returned value. To improve efficiency, the member function with 
+     * the void-returned value is suggested.
+     * 
+     * @param [in] u U coordinate of the 2D point w.r.t SPECIFIED imaging frame.
+     * @param [in] v V coordinate of the 2D point w.r.t SPECIFIED imaging frame.
+     * @param [in] depth Depth of the given 2D point w.r.t GLOBAL camera frame.
+     * @param [in] id    Specify the camera index for binocular.
+     * 
+     * @return A 3D point w.r.t GLOBAL camera frame.
      */
     Eigen::Vector<kfloat, 3> cvt2Dto3D(kfloat u, kfloat v, kfloat depth,
                                        cam::ID id) const;
 
 
     /**
-     * @brief lifting 2D point that w.r.t SPECIFIED camera frame to 3D
-     * @param pt2D  2D Points w.r.t specifed camera
-     * @param depth  Depth of the given 2D point
-     * @param id  Specify the camera index for binocular
-     * @return 3D point w.r.t SPECIFIED camera frame
+     * @brief Lifting 2D point that w.r.t SPECIFIED imaging frame to 3D.
+     * 
+     * @remark This is an overloaded member function, provided for convenience. 
+     * It differs from the base function only in what argument(s) it accepts 
+     * and the returned value. 
+     * 
+     * @param [in] pt2D  A 2D point w.r.t SPECIFIED imaging frame.
+     * @param [in] depth Depth of the given 2D point w.r.t GLOBAL camera frame.
+     * @param [in] id    Specify the camera index for binocular.
+     * @param [out] pt3D  A 3D point w.r.t GLOBAL camera frame.
+     */
+    void cvt2Dto3D(const Eigen::Vector<kfloat, 2>& pt2D, kfloat depth, 
+                   cam::ID id, Eigen::Vector<kfloat, 3>& pt3D) const;
+
+
+    /**
+     * @brief Lifting 2D point that w.r.t SPECIFIED imaging frame to 3D.
+     * 
+     * @remark This is an overloaded member function, provided for convenience. 
+     * It differs from the base function only in what argument(s) it accepts 
+     * and the returned value. To improve efficiency, the member function with 
+     * the void-returned value is suggested.
+     * 
+     * @param [in] pt2D  A 2D point w.r.t SPECIFIED imaging frame.
+     * @param [in] depth Depth of the given 2D point w.r.t GLOBAL camera frame.
+     * @param [in] id    Specify the camera index for binocular.
+     * 
+     * @return A 3D point w.r.t GLOBAL camera frame
      */
     Eigen::Vector<kfloat, 3> cvt2Dto3D(const Eigen::Vector<kfloat, 2>& pt2D,
                                        kfloat depth, cam::ID id) const;
--- src/camera_projector.cpp
+++ src/camera_projector.cpp
@@ -23,58 +23,106 @@ CameraProjector::~CameraProjector()
 }
 
 
-Eigen::Vector<kfloat, 2> CameraProjector::cvt3Dto2D(
-        kfloat x, kfloat y, kfloat z) const
-{
-    kfloat u = (x / z) * fxy + cx;
-    kfloat v = (y / z) * fxy + cy;
-    return Eigen::Vector<kfloat, 2>(u, v);
+void CameraProjector::cvt3Dto2D(kfloat x, kfloat y, kfloat z, 
+        Eigen::Vector<kfloat, 2>& pt2D) const {
+    pt2D[0] = (x / z) * fxy + cx;
+    pt2D[1] = (y / z) * fxy + cy;
 }
 
 
 Eigen::Vector<kfloat, 2> CameraProjector::cvt3Dto2D(
-        const Eigen::Vector<kfloat, 3> &pt3D) const
-{
-    return cvt3Dto2D(pt3D[0], pt3D[1], pt3D[2]);
+        kfloat x, kfloat y, kfloat z) const {
+    Eigen::Vector<kfloat, 2> pt2D;
+    cvt3Dto2D(x, y, z, pt2D);
+    return pt2D;
+}
+
+
+void CameraProjector::cvt3Dto2D(const Eigen::Vector<kfloat, 3>& pt3D,
+        Eigen::Vector<kfloat, 2>& pt2D) const {
+    cvt3Dto2D(pt3D[0], pt3D[1], pt3D[2], pt2D);
 }
 
 
 Eigen::Vector<kfloat, 2> CameraProjector::cvt3Dto2D(
-        kfloat x, kfloat y, kfloat z, cam::ID id) const
-{
+        const Eigen::Vector<kfloat, 3> &pt3D) const {
+    Eigen::Vector<kfloat, 2> pt2D;
+    cvt3Dto2D(pt3D, pt2D);
+    return pt2D;
+}
+
+
+void CameraProjector::cvt3Dto2D(kfloat x, kfloat y, kfloat z, cam::ID id,
+        Eigen::Vector<kfloat, 2>& pt2D) const {
     kfloat x_new = id == cam::LEFT ? x + t/2.f : x - t/2.f;
-    kfloat u = (x_new / z) * fxy + cx;
-    kfloat v = (y / z) * fxy + cy;
-    return Eigen::Vector<kfloat, 2>(u, v);
+    pt2D[0] = (x_new / z) * fxy + cx;
+    pt2D[1] = (y / z) * fxy + cy;
 }
 
 
 Eigen::Vector<kfloat, 2> CameraProjector::cvt3Dto2D(
-        const Eigen::Vector<kfloat, 3> &pt3D, cam::ID id) const
-{
-    return cvt3Dto2D(pt3D[0], pt3D[1], pt3D[2], id);
+        kfloat x, kfloat y, kfloat z, cam::ID id) const {
+    Eigen::Vector<kfloat, 2> pt2D;
+    cvt3Dto2D(x, y, z, id, pt2D);
+    return pt2D;
+}
+
+
+void CameraProjector::cvt3Dto2D(const Eigen::Vector<kfloat, 3>& pt3D, 
+        cam::ID id, Eigen::Vector<kfloat, 2>& pt2D) const {
+    cvt3Dto2D(pt3D[0], pt3D[1], pt3D[2], id, pt2D);
+}
+
+
+Eigen::Vector<kfloat, 2> CameraProjector::cvt3Dto2D(
+        const Eigen::Vector<kfloat, 3> &pt3D, cam::ID id) const {
+    Eigen::Vector<kfloat, 2> pt2D;
+    cvt3Dto2D(pt3D, id), pt2D;
+    return pt2D;
+}
+
+
+void CameraProjector::cvt2Dto3D(kfloat u, kfloat v, kfloat depth,
+    Eigen::Vector<kfloat, 3>& pt3D) const {
+    pt3D[0] = (u - cx) / fxy * depth;
+    pt3D[1] = (v - cy) / fxy * depth;
+    pt3D[2] = depth;
 }
 
 
 Eigen::Vector<kfloat, 3> CameraProjector::cvt2Dto3D(
-        kfloat u, kfloat v, kfloat depth) const
-{
+        kfloat u, kfloat v, kfloat depth) const {
+    Eigen::Vector<kfloat, 3> pt3D;
+    cvt2Dto3D(u, v, depth, pt3D);
+    return pt3D;
+}
+
+
+void CameraProjector::cvt2Dto3D(const Eigen::Vector<kfloat, 2>& pt2D, 
+        kfloat depth, Eigen::Vector<kfloat, 3>& pt3D) const {
+    cvt2Dto3D(pt2D[0], pt2D[1], depth, pt3D);
+}
+
+
+Eigen::Vector<kfloat, 3> CameraProjector::cvt2Dto3D(
+        const Eigen::Vector<kfloat, 2>& pt2D, kfloat depth) const {
+    Eigen::Vector<kfloat, 3> pt3D;
+    cvt2Dto3D(pt2D, depth, pt3D);
+    return pt3D;
+}
+
+
+void CameraProjector::cvt2Dto3D(kfloat u, kfloat v, kfloat depth, cam::ID id, 
+        Eigen::Vector<kfloat, 3>& pt3D) const {
     kfloat x = (u - cx) / fxy * depth;
-    kfloat y = (v - cy) / fxy * depth;
-    return Eigen::Vector<kfloat, 3>(x, y, depth);
+    pt3D[0] = id == cam::LEFT ? x - t/2.f : x + t/2.f;
+    pt3D[1] = (v - cy) / fxy * depth;
+    pt3D[2] = depth;
 }
 
 
 Eigen::Vector<kfloat, 3> CameraProjector::cvt2Dto3D(
-        const Eigen::Vector<kfloat, 2> &pt2D, kfloat depth) const
-{
-    return cvt2Dto3D(pt2D[0], pt2D[1], depth);
-}
-
-
-Eigen::Vector<kfloat, 3> CameraProjector::cvt2Dto3D(
-        kfloat u, kfloat v, kfloat depth, cam::ID id) const
-{
+        kfloat u, kfloat v, kfloat depth, cam::ID id) const {
     kfloat x = (u - cx) / fxy * depth;
     kfloat y = (v - cy) / fxy * depth;
     return id == cam::LEFT ?
@@ -82,6 +130,11 @@ Eigen::Vector<kfloat, 3> CameraProjector::cvt2Dto3D(
                 Eigen::Vector<kfloat, 3>(x + t/2.f, y, depth);
 }
 
+
+void CameraProjector::cvt2Dto3D(const Eigen::Vector<kfloat, 2>& pt2D, 
+        kfloat depth, cam::ID id, Eigen::Vector<kfloat, 3>& pt3D) const {
+    cvt2Dto3D(pt2D[0], pt2D[1], depth, id, pt3D);
+}
 
 Eigen::Vector<kfloat, 3> CameraProjector::cvt2Dto3D(
         const Eigen::Vector<kfloat, 2> &pt2D, kfloat depth, cam::ID id) const
